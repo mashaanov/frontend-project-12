ee08ff4e97503cfe24ff608deda238d5d89b1186
Maria Novikova <m.v.novikovaaa@gmail.com>
2025-02-28 15:51:21 +0400

    update

--- frontend/src/components/ChatArea/ChatArea.jsx
+++ frontend/src/components/ChatArea/ChatArea.jsx
@@ -90,7 +90,6 @@ const ChatArea = () => {
                 <b># {channels.entities[activeChannelId]?.name}</b>
               </p>
               <span className="text-muted">
-                {messages.length}{" "}
                 {console.log("messages.length", messages.length)}
                 {t("messagesCount", { count: messages.length })}
               </span>
--- frontend/src/i18n.js
+++ frontend/src/i18n.js
@@ -1,6 +1,6 @@
 import i18n from "i18next";
 import { initReactI18next } from "react-i18next";
-import translationRU from "./locales/ru.json";
+import translationRU from "./locales/ru";
 
 i18n
   .use(initReactI18next) // Инициализируем react-i18next
