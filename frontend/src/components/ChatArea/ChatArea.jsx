8b6a4beddda641fafe1a2daf8d33ba1b65f868dc
Maria Novikova <m.v.novikovaaa@gmail.com>
2025-03-01 14:17:14 +0400

    update

--- frontend/src/components/ChatArea/ChatArea.jsx
+++ frontend/src/components/ChatArea/ChatArea.jsx
@@ -102,7 +102,15 @@ const ChatArea = () => {
               key={msg.id}
               className="text-break mb-2 d-flex align-items-center justify-content-between"
             >
-              <b>{msg.username}</b>: {msg.body}
+              <b>{msg.username}:</b> {msg.body}
+              <button
+                className="btn btn-link text-danger p-0 ms-2"
+                onClick={() => {
+                  dispatch(removeMessage(msg.id));
+                }}
+              >
+                <FiTrash2 size={18} />
+              </button>
             </div>
           ))}
         </div>
