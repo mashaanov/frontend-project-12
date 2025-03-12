9a5eeb9ba72dafd960a659c4f295cce5fcd612be
Maria Novikova <m.v.novikovaaa@gmail.com>
2025-03-13 00:56:23 +0400

    update

--- frontend/src/utils/getPluralMessages.js
+++ frontend/src/utils/getPluralMessages.js
@@ -1,4 +1,4 @@
-export const getPluralMessages = (count) => {
+export default getPluralMessages = (count) => {
   if (count === 0) return '0 сообщений';
   if (count % 10 === 1 && count % 100 !== 11) return `${count} сообщение`;
   if ([2, 3, 4].includes(count % 10) && ![12, 13, 14].includes(count % 100)) {
