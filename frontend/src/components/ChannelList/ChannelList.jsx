8614a39413b165891c2fd73d7d7546f5e39d4e7e
Maria Novikova <m.v.novikovaaa@gmail.com>
2025-03-13 14:57:10 +0400

    update

--- frontend/src/components/ChannelList/ChannelList.jsx
+++ frontend/src/components/ChannelList/ChannelList.jsx
@@ -100,8 +100,9 @@ const ChannelList = () => {
         const generalChannelId = channels.ids.find(
           (channelId) => channels.entities[channelId]?.name === 'general',
         );
-        if (id === activeChannelId && generalChannelId)
+        if (id === activeChannelId && generalChannelId) {
           dispatch(setActiveChannel(generalChannelId));
+        }
         toast.success(t('notifications.channelRemoved')); // Уведомление об успешном удалении
       })
       .catch((error) => {
@@ -145,10 +146,12 @@ const ChannelList = () => {
         onHide={hideModal}
         onSubmit={(data) => {
           if (modalInfo.type === 'addChannel') handleAddChannel(data.name);
-          if (modalInfo.type === 'renameChannel')
+          if (modalInfo.type === 'renameChannel') {
             handleRenameChannel(data.name, modalInfo.channelId);
-          if (modalInfo.type === 'removeChannel')
+          }
+          if (modalInfo.type === 'removeChannel') {
             handleRemoveChannel(modalInfo.channelId);
+          }
         }}
         channelId={modalInfo.channelId}
         currentChannelName={channels.entities[modalInfo.channelId]?.name}
