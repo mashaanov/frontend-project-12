a90aaffca5b7e46bfc99dfb8daa79098561d3cda
Maria Novikova <m.v.novikovaaa@gmail.com>
2025-03-13 17:39:04 +0400

    update

--- frontend/src/components/ChatArea/ChatArea.jsx
+++ frontend/src/components/ChatArea/ChatArea.jsx
@@ -27,8 +27,8 @@ const ChatArea = () => {
   const { username } = useSelector((store) => ({
     username: store.auth.username,
   }));
-  // eslint-disable-next-line implicit-arrow-linebreak
   const selectMessagesByChannelId = (state, channelId) =>
+    // eslint-disable-next-line implicit-arrow-linebreak
     state.chat.messages.byChannelId[channelId] || [];
 
   const { activeChannelId, channels } = useSelector((store) => ({
